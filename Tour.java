c5fbd3a1863842e761ffbf745aebc5747e320ef4
Jens Green Olander <jengr594@student.liu.se>
2014-11-27 20:32:51 +0100

    Fix Tour.

--- Tour.java
+++ Tour.java
@@ -12,15 +12,30 @@ public final class Tour {
 	
 	/** Construct a Hamilton path visiting all nodes using a greedy heuristic in O(n^2). */
 	public static LinkedList<ExplorationTask> greedy(List<ExplorationTask> nodes, EntityID start, StandardWorldModel world) {
+		if (nodes.size() == 0) return new LinkedList<ExplorationTask>();
+		
 		LinkedList<ExplorationTask> tour = new LinkedList<ExplorationTask>();
-		if (nodes.size() == 0) return tour;
 		LinkedList<ExplorationTask> unvisited = new LinkedList<ExplorationTask>(nodes);
 		
 		// Find first node -- special case
+		greedyAdd(start, tour, unvisited, world);
+		// Find remaing nodes
+		while (!unvisited.isEmpty()) {
+			greedyAdd(tour.getLast().goal, tour, unvisited, world);
+		}
+		
+		return tour;
+	}
+
+	/** Greedily adds best node (from lastNode) from unvisited to tour, remove found node from unvisited. */ 
+	private static void greedyAdd(EntityID lastNode, LinkedList<ExplorationTask> tour,
+			LinkedList<ExplorationTask> unvisited, StandardWorldModel world) {
 		ExplorationTask bestTask = null;
 		int bestCost = Integer.MAX_VALUE;
+		
+		// greedily assign shortest addition.
 		for (ExplorationTask candidate : unvisited) {
-			int cost = world.getDistance(start, candidate.goal);
+			int cost = world.getDistance(lastNode, candidate.goal);
 			if (cost < bestCost) {
 				bestTask = candidate;
 				bestCost = cost;
@@ -28,24 +43,6 @@ public final class Tour {
 		}
 		tour.add(bestTask);
 		unvisited.remove(bestTask);
-		
-		
-		// Find remaining nodes
-		while (!unvisited.isEmpty()) {
-			
-			// greedily assign shortest addition.
-			for (ExplorationTask candidate : unvisited) {
-				int cost = world.getDistance(tour.getLast().goal, candidate.goal);
-				if (cost < bestCost) {
-					bestTask = candidate;
-					bestCost = cost;
-				}
-			}
-			tour.add(bestTask);
-			unvisited.remove(bestTask);
-		}
-		
-		return tour;
 	}
 
 }
